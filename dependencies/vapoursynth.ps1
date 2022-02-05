4088de306e6067c5407bc7d7d8b33aa641e6408b
Aetopia <kirbymario22@gmail.com>
2022-02-05 13:19:57 +0530

    Update vapoursynth.ps1

--- dependencies/vapoursynth.ps1
+++ dependencies/vapoursynth.ps1
@@ -1,4 +1,4 @@
 scoop install "https://raw.githubusercontent.com/ScoopInstaller/Versions/master/bucket/python38.json"
 reg.exe import "$env:USERPROFILE/scoop/apps/python38/current/install-pep-514.reg"
-curl.exe -# "https://raw.githubusercontent.com/Aetopia/Bucket/main/dependencies/vspipe.cmd" -o "env:LOCALAPPDATA\Microsoft\WindowsApps\vspipe.cmd"
-curl.exe -# "https://raw.githubusercontent.com/Aetopia/Bucket/main/dependencies/vsrepo.cmd" -o "env:LOCALAPPDATA\Microsoft\WindowsApps\vsrepo.cmd"
+curl.exe -# "https://raw.githubusercontent.com/Aetopia/Bucket/main/dependencies/vspipe.cmd" -o "$env:LOCALAPPDATA\Microsoft\WindowsApps\vspipe.cmd"
+curl.exe -# "https://raw.githubusercontent.com/Aetopia/Bucket/main/dependencies/vsrepo.cmd" -o "$env:LOCALAPPDATA\Microsoft\WindowsApps\vsrepo.cmd"
