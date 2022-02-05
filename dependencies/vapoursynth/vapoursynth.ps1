105b5b8a0ad6484e1e8c496bfa13437b5965172e
Aetopia <kirbymario22@gmail.com>
2022-02-05 17:39:01 +0530

    Update vapoursynth.ps1

--- dependencies/vapoursynth/vapoursynth.ps1
+++ dependencies/vapoursynth/vapoursynth.ps1
@@ -2,4 +2,3 @@ scoop install "https://raw.githubusercontent.com/ScoopInstaller/Versions/master/
 reg.exe import "$env:USERPROFILE/scoop/apps/python39/current/install-pep-514.reg"
 curl.exe -# "https://raw.githubusercontent.com/Aetopia/Bucket/main/dependencies/vapoursynth/vspipe.cmd" -o "$env:LOCALAPPDATA\Microsoft\WindowsApps\vspipe.cmd"
 curl.exe -# "https://raw.githubusercontent.com/Aetopia/Bucket/main/dependencies/vapoursynth/vsrepo.cmd" -o "$env:LOCALAPPDATA\Microsoft\WindowsApps\vsrepo.cmd"
-cmd /c vsrepo.cmd install ffms2 havsfunc
