bf6c68b393e184c5f29434461a0c945a981b987e
Aetopia <41850963+Aetopia@users.noreply.github.com>
2022-04-17 16:46:30 +0530

    Update lunarclient.ps1

--- lunarclient/lunarclient.ps1
+++ lunarclient/lunarclient.ps1
@@ -1,3 +1,13 @@
 # Post Install Script for Lunar Client. (Scoop.)
+$ErrorActionPreference = "SilentlyContinue"
+$LunarClient = (scoop.cmd which scoop |Split-Path|Split-Path|Split-Path|Split-Path|Split-Path) + '\apps\lunarclient\current'
 
-Remove-Item "$env:USERPROFILE\scoop\apps\lunarclient\current\resources\app-update.yml"
+Remove-Item "$LunarClient\resources\app-update.yml"
+New-Item -Type Directory "$LunarClient\data" 
+if (-not(Test-Path -Path "$env:USERPROFILE\.lunarclient")) {
+    if ((Get-Item "$env:USERPROFILE\.lunarclient").LinkType -ne "SymbolicLink") {
+        Copy-Item -Path "$env:USERPROFILE\.lunarclient" "$LunarClient\data" -Recurse
+        Remove-Item -Path "$env:USERPROFILE\.lunarclient" -Recurse
+        mklink.exe "$env:USERPROFILE\.lunarclient" "$LunarClient\data" /D
+    }    
+}
